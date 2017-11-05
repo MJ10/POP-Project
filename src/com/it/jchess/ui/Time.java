ef1a8516ef295bddfa87f511ad558a9c96f3f954
Moksh Jain <mokshjn00@gmail.com>
2017-11-05 14:49:02 +0530

    added timer class

---
+++ src/com/it/jchess/ui/Time.java
@@ -0,0 +1,50 @@
+package com.it.jchess.ui;
+
+import com.it.jchess.Game;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * This class defines the Timer for the game.
+ */
+public class Time {
+
+    private JLabel label;
+    public Timer timer;
+    int timeRemaining;
+
+    public Time(JLabel label) {
+        timer = new Timer(1000, new TimerListener());
+        this.label = label;
+        this.timeRemaining = Game.timeRemaining;
+    }
+
+    public void start() {
+        timer.start();
+    }
+
+    public void reset() {
+        timeRemaining = Game.timeRemaining;
+    }
+
+    class TimerListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            int minutes, seconds;
+            if (timeRemaining > 0) {
+                minutes = timeRemaining / 60;
+                seconds = timeRemaining % 60;
+                label.setText(String.valueOf(minutes) + ":" + (seconds >= 10 ? String.valueOf(seconds) : "0" + String.valueOf(seconds)));
+                timeRemaining--;
+            } else {
+                label.setText("Time's Up!");
+                reset();
+                start();
+                Game.gameBoard.switchTurns();
+            }
+        }
+    }
+}
