9a4f73d63b1d95364fa121ac28ad8b8a250ad1cb
Moksh Jain <mokshjn00@gmail.com>
2017-11-05 14:49:50 +0530

    king changes

--- src/com/it/jchess/pieces/King.java
+++ src/com/it/jchess/pieces/King.java
@@ -7,14 +7,39 @@ import java.util.ArrayList;
 
 public class King extends Piece {
 
-    public King(String id, String path, int color) {
+    private int kX, kY;
+
+    public King(String id, String path, int color, int locKX, int locKY) {
         setId(id);
         setPath(path);
         setColor(color);
+
+        setLocX(locKX);
+        setLocY(locKY);
     }
 
     @Override
     public ArrayList<Tile> movePiece(Tile[][] pos, int locX, int locY) {
         return null;
+    }
+
+    public boolean isInDanger(Tile boardState[][]) {
+        return false;
+    }
+
+    public int getLocY() {
+        return kY;
+    }
+
+    public void setLocY(int kY) {
+        this.kY = kY;
+    }
+
+    public int getLocX() {
+        return kX;
+    }
+
+    public void setLocX(int kX) {
+        this.kX = kX;
     }
 }
